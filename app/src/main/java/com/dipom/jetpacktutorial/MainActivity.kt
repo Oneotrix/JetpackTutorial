2162f8d85eb8b7dee18e10ffe9dd194eb7e98ce7
Максим Шутов <m5555hutov@gmail.com>
2023-09-02 21:39:23 +0300

    add list - lazyColumn into project, add dataset for this list

--- app/src/main/java/com/dipom/jetpacktutorial/MainActivity.kt
+++ app/src/main/java/com/dipom/jetpacktutorial/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.dipom.jetpacktutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.os.Message
 import androidx.activity.ComponentActivity
@@ -15,6 +16,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -25,6 +28,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.dipom.jetpacktutorial.data.SampleData
 import com.dipom.jetpacktutorial.ui.theme.JetpackTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -66,7 +70,7 @@ fun MessageCard(msg: com.dipom.jetpacktutorial.models.Message) {
             
             Spacer(modifier = Modifier.height(5.dp))
             
-            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+            Surface(shape = MaterialTheme.shapes.large, shadowElevation = 3.dp) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
@@ -76,12 +80,34 @@ fun MessageCard(msg: com.dipom.jetpacktutorial.models.Message) {
     }
 }
 
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PrevMessageCard() {
     JetpackTutorialTheme {
         Surface {
             MessageCard(com.dipom.jetpacktutorial.models.Message("Maks", "Hello world"))
         }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<com.dipom.jetpacktutorial.models.Message>) {
+    LazyColumn {
+        items(messages) {
+            MessageCard(msg = it)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PrevConversation() {
+    JetpackTutorialTheme {
+        Conversation(SampleData.conversationSample)
     }
 }
