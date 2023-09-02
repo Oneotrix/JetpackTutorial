4fd86375bcb87b7c28a937cdcc9ac4a77ed45882
Максим Шутов <m5555hutov@gmail.com>
2023-09-02 20:44:35 +0300

    transform image to circle image, wor with Spacer and Modifier

--- app/src/main/java/com/dipom/jetpacktutorial/MainActivity.kt
+++ app/src/main/java/com/dipom/jetpacktutorial/MainActivity.kt
@@ -8,14 +8,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,11 +41,20 @@ fun MessageCard(msg: com.dipom.jetpacktutorial.models.Message) {
     Row {
         Image(  painter = painterResource(id = R.drawable.photo),
                 contentDescription = "Maks photo",
-                modifier = Modifier.size(width = 100.dp, height = 100.dp))
-
+                modifier = Modifier
+                    //set size
+                    .size(40.dp)
+                    //clip to circle
+                    .clip(CircleShape)
+        )
+    
+        Spacer(modifier = Modifier.width(10.dp))
     
         Column() {
             Text(text = msg.author)
+            
+            Spacer(modifier = Modifier.height(5.dp))
+            
             Text(text = msg.body)
          }
     }
