ceba4f98c0637ab0d8d4028ba4147395b042076c
Максим Шутов <m5555hutov@gmail.com>
2023-09-02 20:38:46 +0300

    add image

--- app/src/main/java/com/dipom/jetpacktutorial/MainActivity.kt
+++ app/src/main/java/com/dipom/jetpacktutorial/MainActivity.kt
@@ -4,16 +4,21 @@ import android.os.Bundle
 import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dipom.jetpacktutorial.ui.theme.JetpackTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,9 +34,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MessageCard(msg: com.dipom.jetpacktutorial.models.Message) {
-    Box {
-        Text(text = msg.author)
-        Text(text = msg.body)
+    Row {
+        Image(  painter = painterResource(id = R.drawable.photo),
+                contentDescription = "Maks photo",
+                modifier = Modifier.size(width = 100.dp, height = 100.dp))
+
+    
+        Column() {
+            Text(text = msg.author)
+            Text(text = msg.body)
+         }
     }
 }
 
