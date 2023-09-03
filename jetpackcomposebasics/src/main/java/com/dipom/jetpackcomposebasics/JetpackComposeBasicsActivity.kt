1d21d44c6644fedbbe168030aa5fc01edff8bc99
Максим Шутов <m5555hutov@gmail.com>
2023-09-03 21:28:20 +0300

    replace remember to rememberSaveable for save card expanded state wile scorolling and etc

--- jetpackcomposebasics/src/main/java/com/dipom/jetpackcomposebasics/JetpackComposeBasicsActivity.kt
+++ jetpackcomposebasics/src/main/java/com/dipom/jetpackcomposebasics/JetpackComposeBasicsActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -42,7 +43,7 @@ class JetpackComposeBasicsActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
 
-    val expanded = remember { mutableStateOf(false)}
+    val expanded = rememberSaveable { mutableStateOf(false)}
     val extraPadding = if (expanded.value) 48.dp else 0.dp
 
     Surface(color = MaterialTheme.colorScheme.primary,
