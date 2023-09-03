bc9ac3f8a473a45b77732707c680fa090ae19c08
Максим Шутов <m5555hutov@gmail.com>
2023-09-03 22:02:47 +0300

    work with themes and colors, have bugs, fix later

--- jetpackcomposebasics/src/main/java/com/dipom/jetpackcomposebasics/JetpackComposeBasicsActivity.kt
+++ jetpackcomposebasics/src/main/java/com/dipom/jetpackcomposebasics/JetpackComposeBasicsActivity.kt
@@ -1,5 +1,6 @@
 package com.dipom.jetpackcomposebasics
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -28,6 +29,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dipom.jetpackcomposebasics.ui.theme.JetpackTutorialTheme
@@ -64,7 +66,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 .padding(bottom = extraPadding.coerceAtLeast(0.dp)))
             {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
             }
 
             ElevatedButton(onClick = { expanded = !expanded },
@@ -145,6 +149,11 @@ fun GreetingPreview() {
 }
 
 @Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "dark")
 @Composable
 fun PrevGreetings() {
     JetpackTutorialTheme {
