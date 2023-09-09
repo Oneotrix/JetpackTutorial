9200676a0572b844b3584f8f1a2aaabad08b6457
Максим Шутов <m5555hutov@gmail.com>
2023-09-09 18:24:54 +0300

    build search item on 60%

--- ui_kit/src/main/java/com/dipom/ui_kit/MainActivity.kt
+++ ui_kit/src/main/java/com/dipom/ui_kit/MainActivity.kt
@@ -4,12 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dipom.ui_kit.ui.theme.JetpackTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +31,42 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun SearchBar(modifier: Modifier) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+            Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        ),
+        placeholder = {
+            Text(text = "Hello")
+        },
         modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp)
     )
+
 }
 
-@Preview(showBackground = true)
+
+@Preview
 @Composable
-fun GreetingPreview() {
+fun SearchBarPreview() {
     JetpackTutorialTheme {
-        Greeting("Android")
+        Surface(color = MaterialTheme.colorScheme.background) {
+            SearchBar(Modifier)
+        }
     }
 }
