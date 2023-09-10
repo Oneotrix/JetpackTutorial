572e88ea363a343f6f95f555e4b1d497891f8955
Максим Шутов <m5555hutov@gmail.com>
2023-09-10 11:26:08 +0300

    make AlignYourBodyElement

--- ui_kit/src/main/java/com/dipom/ui_kit/MainActivity.kt
+++ ui_kit/src/main/java/com/dipom/ui_kit/MainActivity.kt
@@ -3,9 +3,14 @@ package com.dipom.ui_kit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -16,7 +21,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dipom.ui_kit.ui.theme.JetpackTutorialTheme
@@ -51,7 +61,7 @@ fun SearchBar(modifier: Modifier) {
             containerColor = MaterialTheme.colorScheme.surface
         ),
         placeholder = {
-            Text(text = "Hello")
+            Text(text = "Search")
         },
         modifier = modifier
             .fillMaxWidth()
@@ -60,6 +70,24 @@ fun SearchBar(modifier: Modifier) {
 
 }
 
+@Composable
+fun AlignYourBodyElement(modifier: Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            contentScale = ContentScale.Crop,
+            painter = painterResource(id = R.drawable.ab1_inversions),
+            contentDescription = null,
+            modifier = modifier
+                .size(88.dp)
+                .clip(CircleShape)
+        )
+        Text(text = stringResource(id = R.string.adb1_inversions),
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp),
+            style = MaterialTheme.typography.bodyMedium)
+    }
+}
 
 @Preview
 @Composable
@@ -67,6 +95,16 @@ fun SearchBarPreview() {
     JetpackTutorialTheme {
         Surface(color = MaterialTheme.colorScheme.background) {
             SearchBar(Modifier)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AlignYourBodyElementPreview() {
+    JetpackTutorialTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            AlignYourBodyElement(Modifier)
         }
     }
 }
