98611edcce4e68589304675d3c27d82878fb38cd
Максим Шутов <m5555hutov@gmail.com>
2023-09-14 00:08:47 +0300

    create WellnessTaskItem, that contain text and image button

--- state/src/main/java/com/dipom/state/WaterCounter.kt
+++ state/src/main/java/com/dipom/state/WaterCounter.kt
@@ -23,12 +23,18 @@ fun WaterCounter(
 
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0)}
-        Text(
-            text = "You've have $count glasses."
-        )
+        if(count > 0) {
+            WellnessTaskItem(
+                taskName = "Have you taken your 15 minute walk today?",
+                onClose = { }
+            )
+            Text(text = "You've had $count glasses.")
+        }
+
         Button(
             onClick = { count ++},
-            modifier = Modifier.padding(top = 8.dp)
+            modifier = Modifier.padding(top = 8.dp),
+            enabled = count < 10
         ) {
             Text(text = "Add one")
         }
