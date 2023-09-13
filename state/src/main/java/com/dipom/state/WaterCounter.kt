8dcc7dd28c5096af10fd9321841e079171196f83
Максим Шутов <m5555hutov@gmail.com>
2023-09-13 22:21:42 +0300

    add remember in count property declaration(fix bug, that doesn't save state of count)

--- state/src/main/java/com/dipom/state/WaterCounter.kt
+++ state/src/main/java/com/dipom/state/WaterCounter.kt
@@ -6,6 +6,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -17,12 +20,12 @@ fun WaterCounter(
 ) {
 
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
+        val count: MutableState<Int> = remember {mutableStateOf(0)}
         Text(
-            text = "You've have $count glasses."
+            text = "You've have ${count.value} glasses."
         )
         Button(
-            onClick = { count++ },
+            onClick = { count.value ++},
             modifier = Modifier.padding(top = 8.dp)
         ) {
             Text(text = "Add one")
