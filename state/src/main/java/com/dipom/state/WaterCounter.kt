39f71d4474184c1325430c46217fac703d3eae74
Максим Шутов <m5555hutov@gmail.com>
2023-09-13 22:26:29 +0300

    simplify count initialization by using delegate

--- state/src/main/java/com/dipom/state/WaterCounter.kt
+++ state/src/main/java/com/dipom/state/WaterCounter.kt
@@ -7,8 +7,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,12 +22,12 @@ fun WaterCounter(
 ) {
 
     Column(modifier = modifier.padding(16.dp)) {
-        val count: MutableState<Int> = remember {mutableStateOf(0)}
+        var count by remember { mutableStateOf(0)}
         Text(
-            text = "You've have ${count.value} glasses."
+            text = "You've have $count glasses."
         )
         Button(
-            onClick = { count.value ++},
+            onClick = { count ++},
             modifier = Modifier.padding(top = 8.dp)
         ) {
             Text(text = "Add one")
