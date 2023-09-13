a348a5e7aacd2b3811d21fbf3191b179b749c429
Максим Шутов <m5555hutov@gmail.com>
2023-09-14 00:35:44 +0300

    replace remember by rememberSaveable to save state when configuration change

--- state/src/main/java/com/dipom/state/WaterCounter.kt
+++ state/src/main/java/com/dipom/state/WaterCounter.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,7 +24,7 @@ fun WaterCounter(
 ) {
 
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0)}
+        var count by rememberSaveable { mutableStateOf(0)}
         if(count > 0) {
 //            var showTask by remember { mutableStateOf(true) }
 //            if(showTask) {
