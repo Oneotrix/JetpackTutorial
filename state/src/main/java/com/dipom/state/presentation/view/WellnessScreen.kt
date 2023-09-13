a4fa65b698b933011e5121d9f5ad08a84f49c74e
Максим Шутов <m5555hutov@gmail.com>
2023-09-14 02:14:52 +0300

    update WellnessScrreen to use viewModel

--- state/src/main/java/com/dipom/state/presentation/view/WellnessScreen.kt
+++ state/src/main/java/com/dipom/state/presentation/view/WellnessScreen.kt
@@ -5,21 +5,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.dipom.state.data.WellnessTask
+import com.dipom.state.presentation.viewmodel.WellnessViewModel
 
 @Composable
 fun WellnessScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
 ) {
     Column(modifier) {
         StatefulCounter(modifier = modifier)
 
-        val list = remember { getWellnessTasks().toMutableStateList() }
         WellnessTaskList(
-            list = list,
-            onClouseTask = {task -> list.remove(task)}
+            list = wellnessViewModel.tasks,
+            onClouseTask = {task -> wellnessViewModel.remove(task)}
         )
     }
 }
 
-private fun getWellnessTasks() = List(30) {i -> WellnessTask(i, "Task #$i") }
