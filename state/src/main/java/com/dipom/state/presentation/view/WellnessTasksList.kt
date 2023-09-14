f1e165c0b155952c7b7dea996468acb9d6970f18
Максим Шутов <m5555hutov@gmail.com>
2023-09-14 15:15:35 +0300

    remove init value from WellnessTaskList param list

--- state/src/main/java/com/dipom/state/presentation/view/WellnessTasksList.kt
+++ state/src/main/java/com/dipom/state/presentation/view/WellnessTasksList.kt
@@ -3,18 +3,20 @@ package com.dipom.state.presentation.view
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.RememberObserver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import com.dipom.state.data.WellnessTask
 
-private fun getWellnessTasks() = List(30) {i -> WellnessTask(i, "Task #$i")}
+//private fun getWellnessTasks() = List(30) {i -> WellnessTask(i, "Task #$i")}
 
 
 @Composable
 fun WellnessTaskList(
     modifier: Modifier = Modifier,
-    list: List<WellnessTask> = rememberSaveable { getWellnessTasks() },
-    onClouseTask: (WellnessTask) -> Unit
+    list: List<WellnessTask>,
+    onClouseTask: (WellnessTask) -> Unit,
+
 ) {
     LazyColumn(
         modifier = modifier
